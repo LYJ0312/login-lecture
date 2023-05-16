4996b63b2cbb40183ff1195b70506710c640c2b3
LYJ0312 <slfmr0312@mju.ac.kr>
2023-05-16 23:42:33 +0900

    회원가입 페이지 추가

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 // 서버에서 로그인 데이터 받음
 router.post('/login', ctrl.process.login);
 
