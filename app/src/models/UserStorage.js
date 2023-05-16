4af4e8fec9e19bbb80c4b055ac0765b80668cf24
LYJ0312 <slfmr0312@mju.ac.kr>
2023-05-16 22:08:55 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class UserStorage{
+    // 임시로 데이터 넣어놓음
+    // 정적변수(static)로 만들어 다른 클래스에서 접근 가능하게
+    // 외부에서 못받게 #을 붙여줌. 데이터 은닉화
+    static #users = {
+        id: ["김이박", "이찬수", "최형실"],
+        password: ["1111", "2222", "3333"],
+        name: ["김김김", "이이이", "박박박"],
+    };
+
+    // 데이터 은닉화 했으니까, 불러오기 위해 get설정
+    // 변수 몇 개를 요구할지 모르니 ...변수명 으로 설정
+    static getUsers(...fields){
+        const users =this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if(users.hasOwnProperty(field)){ // 키 값이 있으면
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
+
 
 const output = {
     home: (req, res)=>{
@@ -11,11 +13,6 @@ const output = {
     },
 };
 
-// 임시로 데이터 넣어놓음
-const users = {
-    id: ["aaa", "bbb", "ccc"],
-    password: ["111", "222", "333"],
-};
 
 // body보려면 app.js에서 모듈 설치해야 함
 // 들어온 데이터가 저장되어 있는것과 일치한지 아닌지 확인
@@ -23,20 +20,23 @@ const process = {
     login: (req, res) =>{
         const id = req.body.id,
         password = req.body.password;
+        // const userStorage = new UserStorage();
 
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password){
-                return res.json({
-                    success: true,
-                });
-            }
-        }
+        // UserStorage에서 static으로 해놔서 접근 가능함.
+        console.log(UserStorage.getUsers("id", "password", "name"));
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        })
+        const response = {};
+        // if(users.id.includes(id)){
+        //     const idx = users.id.indexOf(id);
+        //     if(users.password[idx] === password){
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
+
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
     },
 };
 
